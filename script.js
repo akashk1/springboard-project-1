76fbf665d1d83b572c4d33906545088fc28ebfb9
Akash Kumar Singh <akashkumarsingh3@gmail.com>
2023-05-24 21:08:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu"
+];
+
+function search(str) {
+  let results = [];
+
+  for (let i = 0; i < fruit.length; i++) {
+    const lowercaseFruit = fruit[i].toLowerCase();
+    const lowercaseStr = str.toLowerCase();
+
+    if (lowercaseFruit.includes(lowercaseStr)) {
+      results.push(fruit[i]);
+    }
+  }
+
+  return results;
+}
+
+function searchHandler(e) {
+  const inputVal = e.target.value;
+  const results = search(inputVal);
+  showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+  if (results.length === 0 || inputVal.length === 0) {
+    suggestions.innerHTML = "";
+    suggestions.classList.remove("has-suggestions");
+    return;
+  }
+
+  const suggestionsList = results
+    .map((result) => `<li>${result}</li>`)
+    .join("");
+
+  console.log(suggestionsList);
+  suggestions.innerHTML = suggestionsList;
+  suggestions.classList.add("has-suggestions");
+}
+
+function useSuggestion(e) {
+  if (e.target.tagName === "LI") {
+    const selectedSuggestion = e.target.innerText;
+    input.value = selectedSuggestion;
+  }
+}
+
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
